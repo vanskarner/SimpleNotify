0d9c73bf2c3e6f5ff0cfc04609c779bb2ae86a59
Vanskarner <vanskarner@gmail.com>
2024-09-12 00:00:08 -0500

    notification examples

--- app/src/main/java/com/vanskarner/simplenotify/MainActivity.kt
+++ app/src/main/java/com/vanskarner/simplenotify/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.vanskarner.simplenotify
 
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : Activity() {
 
@@ -16,7 +26,48 @@ class MainActivity : Activity() {
     }
 
     private fun notificationType1() {
+        val channelId = "default_channel"
+        val smallIcon = R.drawable.baseline_notifications_24
+        val title = "textTitle"
+        val text = "textContent"
+        // Create an explicit intent for an Activity in your app.
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
+        var builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(smallIcon)
+            .setContentTitle(title)
+            .setContentText(text)
+//            .setStyle(NotificationCompat.BigTextStyle()
+//                .bigText("Much longer text that cannot fit one line..."))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        createNotificationChannel(channelId)
+
+//        with(NotificationManagerCompat.from(this)) {
+//            if (ActivityCompat.checkSelfPermission(
+//                    this@MainActivity,
+//                    Manifest.permission.POST_NOTIFICATIONS
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                // TODO: Consider calling
+//                // ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                // public fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+//                //                                        grantResults: IntArray)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//
+//                return@with
+//            }
+//            // notificationId is a unique int for each notification that you must define.
+//            notify(NOTIFICATION_ID, builder.build())
+//        }
     }
 
     private fun notificationType2() {
@@ -30,4 +81,22 @@ class MainActivity : Activity() {
     private fun notificationType4() {
 
     }
+
+    private fun createNotificationChannel(channelId: String) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "getString(R.string.channel_name)"
+            val descriptionText = "getString(R.string.channel_description)"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 }
