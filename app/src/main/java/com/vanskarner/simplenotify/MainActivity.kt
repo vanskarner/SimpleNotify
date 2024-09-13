436870d8a31dffddca42faa3efbef9d6986739ec
Vanskarner <vanskarner@gmail.com>
2024-09-12 21:27:35 -0500

    first notifications

--- app/src/main/java/com/vanskarner/simplenotify/MainActivity.kt
+++ app/src/main/java/com/vanskarner/simplenotify/MainActivity.kt
@@ -1,28 +1,80 @@
 package com.vanskarner.simplenotify
 
-import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            //If the permit request is rejected twice, the remaining attempts will be false.
+            showPermissionText(isGranted)
+            if (!isGranted) showPermissionDeniedDialog()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.main_activity)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            showPermissionText(hasNotificationPermission())
+            findViewById<Button>(R.id.btnPermission).setOnClickListener {
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+        findViewById<Button>(R.id.btnSetting).setOnClickListener { openSettings() }
         findViewById<Button>(R.id.btnType1).setOnClickListener { notificationType1() }
         findViewById<Button>(R.id.btnType2).setOnClickListener { notificationType2() }
         findViewById<Button>(R.id.btnType3).setOnClickListener { notificationType3() }
         findViewById<Button>(R.id.btnType4).setOnClickListener { notificationType4() }
+
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Notifications permission not granted")
+            .setMessage("You have denied notifications permission. To enable it, go to the app settings.")
+            .setPositiveButton("Go to Settings") { _, _ -> openSettings() }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent)
+    }
+
+    private fun showPermissionText(hasPermission: Boolean) {
+        val permission = if (hasPermission) "Granted" else "Not Granted"
+        findViewById<TextView>(R.id.tvPermission).text = permission
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun hasNotificationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun notificationType1() {
@@ -30,48 +82,36 @@ class MainActivity : Activity() {
         val smallIcon = R.drawable.baseline_notifications_24
         val title = "textTitle"
         val text = "textContent"
-        // Create an explicit intent for an Activity in your app.
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
         val pendingIntent: PendingIntent =
             PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-        var builder = NotificationCompat.Builder(this, channelId)
+        val builder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(smallIcon)
             .setContentTitle(title)
             .setContentText(text)
-//            .setStyle(NotificationCompat.BigTextStyle()
-//                .bigText("Much longer text that cannot fit one line..."))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
 
-        createNotificationChannel(channelId)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) createNotificationChannel(channelId)
 
-//        with(NotificationManagerCompat.from(this)) {
-//            if (ActivityCompat.checkSelfPermission(
-//                    this@MainActivity,
-//                    Manifest.permission.POST_NOTIFICATIONS
-//                ) != PackageManager.PERMISSION_GRANTED
-//            ) {
-//                // TODO: Consider calling
-//                // ActivityCompat#requestPermissions
-//                // here to request the missing permissions, and then overriding
-//                // public fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
-//                //                                        grantResults: IntArray)
-//                // to handle the case where the user grants the permission. See the documentation
-//                // for ActivityCompat#requestPermissions for more details.
-//
-//                return@with
-//            }
-//            // notificationId is a unique int for each notification that you must define.
-//            notify(NOTIFICATION_ID, builder.build())
-//        }
+        val notificationId = 12
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return@with
+            }
+            notify(notificationId, builder.build())
+        }
     }
 
     private fun notificationType2() {
-
     }
 
     private fun notificationType3() {
@@ -82,21 +122,16 @@ class MainActivity : Activity() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(channelId: String) {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is not in the Support Library.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "getString(R.string.channel_name)"
-            val descriptionText = "getString(R.string.channel_description)"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(channelId, name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system.
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+        val name = "Default channel"
+        val descriptionText = "Application default notification channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, name, importance)
+            .apply { description = descriptionText }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 
 }
