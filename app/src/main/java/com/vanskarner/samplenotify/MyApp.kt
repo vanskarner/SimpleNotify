b4ec79bd40e27c58ee4c271b1aec87dbc40c04af
Vanskarner <vanskarner@gmail.com>
2024-10-01 14:12:34 -0500

    inclusion of MyApp

---
+++ app/src/main/java/com/vanskarner/samplenotify/MyApp.kt
@@ -0,0 +1,12 @@
+package com.vanskarner.samplenotify
+
+import android.app.Application
+
+class MyApp : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        //Channel creation
+    }
+
+}
