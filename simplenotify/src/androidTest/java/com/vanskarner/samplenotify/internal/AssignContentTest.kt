84a431a470733df79a626109f4680eacbe155091
Vanskarner <vanskarner@gmail.com>
2024-10-02 23:07:46 -0500

    tests in AssignContent completed

--- simplenotify/src/androidTest/java/com/vanskarner/samplenotify/internal/AssignContentTest.kt
+++ simplenotify/src/androidTest/java/com/vanskarner/samplenotify/internal/AssignContentTest.kt
@@ -1,17 +1,26 @@
 package com.vanskarner.samplenotify.internal
 
 import android.app.Notification
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Icon
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import com.vanskarner.samplenotify.ActionData
+import com.vanskarner.samplenotify.ChannelData
 import com.vanskarner.samplenotify.Data
 import com.vanskarner.samplenotify.ExtraData
+import com.vanskarner.samplenotify.ProgressData
 import com.vanskarner.simplenotify.R
 import org.junit.Assert.*
 import org.junit.Before
@@ -80,6 +89,89 @@ class AssignContentTest {
             notification.extras.getBoolean(NotificationCompat.EXTRA_SHOW_WHEN)
         )
         assertEquals(extraData.useChronometer, NotificationCompat.getUsesChronometer(notification))
+    }
+
+    @Test
+    @Suppress("DEPRECATION")
+    fun applyAction_apply() {
+        val basicActionData = ActionData.BasicAction(
+            icon = R.drawable.baseline_notifications_24,
+            label = "Any Label",
+            pending = createPendingIntent()
+        )
+        val replyAction = ActionData.ReplyAction(
+            icon = R.drawable.baseline_notifications_24,
+            label = "Any Label",
+            replyPending = createPendingIntent(),
+            remote = RemoteInput.Builder("any_key").build()
+        )
+        assignContent.applyAction(basicActionData, builder)
+        assignContent.applyAction(replyAction, builder)
+        val notification = builder.build()
+
+        assertEquals(2, notification.actions.size)
+        assertEquals(basicActionData.icon, notification.actions[0].icon)
+        assertEquals(basicActionData.label, notification.actions[0].title)
+        assertEquals(basicActionData.pending, notification.actions[0].actionIntent)
+        assertEquals(replyAction.icon, notification.actions[1].icon)
+        assertEquals(replyAction.label, notification.actions[1].title)
+        assertEquals(replyAction.replyPending, notification.actions[1].actionIntent)
+        assertNotNull(notification.actions[1].remoteInputs[0])
+    }
+
+    @Test
+    fun applyProgress_whenIsNotHide_apply() {
+        val progressData = ProgressData(
+            currentValue = 50,
+            indeterminate = true,
+            hide = false
+        )
+        assignContent.applyProgress(progressData, builder)
+        val notification = builder.build()
+
+        assertEquals(
+            progressData.currentValue,
+            notification.extras.getInt(NotificationCompat.EXTRA_PROGRESS)
+        )
+        assertEquals(
+            progressData.indeterminate,
+            notification.extras.getBoolean(NotificationCompat.EXTRA_PROGRESS_INDETERMINATE)
+        )
+    }
+
+    @Test
+    fun applyProgress_whenIsHide_apply() {
+        val progressData = ProgressData(
+            currentValue = 50,
+            indeterminate = true,
+            hide = true
+        )
+        assignContent.applyProgress(progressData, builder)
+        val notification = builder.build()
+
+        assertEquals(0, notification.extras.getInt(NotificationCompat.EXTRA_PROGRESS))
+        assertFalse(notification.extras.getBoolean(NotificationCompat.EXTRA_PROGRESS_INDETERMINATE))
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
+    fun applyNotificationChannel_apply() {
+        val channelData = ChannelData(
+            id = "Any Id",
+            name = "Any Name",
+            summary = "Any Summary",
+            importance = NotificationManager.IMPORTANCE_HIGH,
+            sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),
+            audioAttributes = AudioAttributes.Builder().build()
+        )
+        val notificationChannel = assignContent.applyNotificationChannel(channelData)
+
+        assertEquals(channelData.id,notificationChannel.id)
+        assertEquals(channelData.name,notificationChannel.name)
+        assertEquals(channelData.summary,notificationChannel.description)
+        assertEquals(channelData.importance,notificationChannel.importance)
+        assertEquals(channelData.sound,notificationChannel.sound)
+        assertEquals(channelData.audioAttributes,notificationChannel.audioAttributes)
     }
 
     private fun createExtraData() = ExtraData(
