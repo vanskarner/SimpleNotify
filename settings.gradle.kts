ff6e6cfbdec32c36892379167f7010fc019af0f9
Vanskarner <vanskarner@gmail.com>
2024-12-30 20:14:02 -0500

    preparation for the first pull request

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "SimpleNotify"
 include(":app")
-include(":simplenotify")
--- simplenotify/src/main/java/com/vanskarner/samplenotify/internal/DefaultValues.kt
+++
@@ -1,5 +0,0 @@
-package com.vanskarner.samplenotify.internal
-
-internal const val DEFAULT_CHANNEL_ID = "SingleNotifyId"
-internal const val DEFAULT_PROGRESS_CHANNEL_ID = "ProgressSingleNotifyId"
-internal const val MAXIMUM_ACTIONS = 3
